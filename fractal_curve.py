ef2d2d0ce8a95a97fe8316c688e2ccd84e529616
Yuri Malykhin <jura05 at >
2017-12-19 18:50:03 +0300

    further development

--- fractal_curve.py
+++ fractal_curve.py
@@ -1,22 +1,54 @@
 # coding: utf-8
 
+from fractions import Fraction
+
 class BaseMap:
     """Base map: isometry of cube and (possibly) time reversal.
     Params:
-        cube_map   isometry of cube
-                      S=[(k_0,b_0),...,(k_{d-1},b_{d-1})] which defines map
-                      (x_0,...,x_{d-1}) -> (f_{b_0}(x_{k_0}),...,f_{k_{d-1}}(x_{k_{d-1}})),
-                      where f_b(x)=x if b is False, and f_b(x)=1-x otherwise
-        time_rev   time reversal (boolean), default: False
+        perm, flip: params, which define isometry of cube
+                      perm = [k_0,...,k_{d-1}]
+                      flip = [b_0,...,b_{d-1}]
+                      define the map
+                      (x_0,...,x_{d-1}) -> (f(b_0,x_{k_0}),...,f(b_{d-1},x_{k_{d-1}})),
+                      where f(b,x)=x if b is False, and 1-x otherwise
+        time_rev    time reversal (boolean), default: False
     """
-    def __init__(self, cube_map, time_rev=False):
-        self.cube_map = cube_map
-        self.time_rev = time_rev
 
-    def apply(self, x):
+    @classmethod
+    def id_map(cls, dim):
+        perm = list(range(dim))
+        flip = [False]*dim
+        time_rev = False
+        return cls(perm, flip, time_rev)
+
+    def __init__(self, perm, flip, time_rev=False):
+        assert len(perm) == len(flip)
+        self.dim = len(perm)
+        self.perm = perm
+        self.flip = [bool(b) for b in flip]
+        self.time_rev = bool(time_rev)
+
+    def __mul__(self, other):
+        """Composition of base maps."""
+        assert self.dim == other.dim
+        perm = []
+        flip = []
+        for i in range(self.dim):
+            k = other.perm[self.perm[i]]
+            b1 = self.flip[i]
+            b2 = other.flip[self.perm[i]]
+            perm.append(k)
+            flip.append(b1 ^ b2)
+        time_rev = self.time_rev ^ other.time_rev
+        return self.__class__(perm, flip, time_rev)
+
+    def apply_x(self, x):
         """Apply isometry to a point x of [0,1]^d."""
-        return tuple(1-x[ki] if bi else x[ki] for ki, bi in self.cube_map)
+        return [1-x[ki] if bi else x[ki] for ki, bi in zip(self.perm, self.flip)]
 
+    def apply_cube(self, div, cube):
+        """Apply isometry to a sub-cube."""
+        return [div-cube[ki]-1 if bi else cube[ki] for ki, bi in zip(self.perm, self.flip)]
 
 
 class FractalCurve:
@@ -24,20 +56,51 @@ class FractalCurve:
     Params:
         div         positive integer, number of divisions of each side of the cube (characteristic of a curve)
         dim         dimension (default: 2)
-        proto       curve prototype - sequence of "cubes" enumerated as (x_0,..,x_{d-1}), 0<=x_j<div
-        entrance    entrance point
-        exit        exit point
+        proto       curve prototype - sequence of "cubes" with coordinates (x_0,..,x_{d-1}), 0<=x_j<div
         base_maps   sequence of base maps (instances of BaseMap class)
+        entrance    entrance point (default: None, i.e. determine by proto & base_maps)
+        exit        exit point (default: None, see entrance)
 
     For examples see get_hilbert_curve()
     """
-    def __init__(self, div, proto, base_maps, exit, entrance=(0,0), dim=2):
+    def __init__(self, div, proto, base_maps, entrance=None, exit=None, dim=2):
         self.div = div
         self.dim = dim
         self.proto = proto
-        self.entrance = entrance
-        self.exit = exit
         self.base_maps = base_maps
+        self.entrance = entrance if entrance is not None else self.get_entrance()
+        self.exit = exit if exit is not None else self.get_exit()
+
+    def get_entrance(self):
+        return self._get_limit(self.div, self.proto[0], self.base_maps[0])
+
+    def get_exit(self):
+        return self._get_limit(self.div, self.proto[-1], self.base_maps[-1])
+
+    @staticmethod
+    def _get_limit(div, cube, base_map):
+        cube_period = [cube]
+        cur_map = base_map
+        # составляем номера под-кубов (в изначальной ориентации)
+        # каждый следующий куб получается умножением слева на base_map
+        while True:
+            new_cube = cur_map.apply_cube(div, cube)
+            cur_map = base_map * cur_map
+            if tuple(new_cube) == tuple(cube):
+                break
+            cube_period.append(new_cube)
+
+        # суммируем геометрическую прогрессию (левые нижние углы кубов) со знаменателем div**(-m)
+        m = len(cube_period)
+        x = 0
+        y = 0
+        for i, c in enumerate(cube_period):
+            x += c[0] * div**(m-1-i) 
+            y += c[1] * div**(m-1-i)
+
+        lx = Fraction(x, div**m - 1)
+        ly = Fraction(y, div**m - 1)
+        return (lx,ly)
 
     def genus(self):
         """Fractal genus of the curve."""
@@ -67,9 +130,9 @@ class FractalCurve:
         entrance_n = tuple(n*self.entrance[j] for j in range(d))  # начальный вход
         gates.append((None, entrance_n))
         for cube, bm in zip(self.proto, self.base_maps):
-            entrance_pos = bm.apply(self.entrance)
+            entrance_pos = bm.apply_x(self.entrance)
             entrance_n = tuple(cube[j] + entrance_pos[j] for j in range(d))
-            exit_pos = bm.apply(self.exit)
+            exit_pos = bm.apply_x(self.exit)
             exit_n = tuple(cube[j] + exit_pos[j] for j in range(d))
             if bm.time_rev:
                 gates.append((exit_n,entrance_n))
@@ -80,44 +143,3 @@ class FractalCurve:
 
         for i in range(len(gates)-1):
             assert gates[i][1] == gates[i+1][0], 'exit does not correspond to entrance'
-
-
-def get_hilbert_curve():
-    """Example of fractal curve due to D.Hilbert."""
-    return FractalCurve(
-        dim=2, div=2,
-        entrance=(0,0), exit=(1,0),
-        proto=[(0,0), (0,1), (1,1), (1,0)],
-        base_maps=[
-            BaseMap([(1,False),(0,False)]),  # (x,y)->(y,x)
-            BaseMap([(0,False),(1,False)]),  # (x,y)->(x,y)
-            BaseMap([(0,False),(1,False)]),  # (x,y)->(x,y)
-            BaseMap([(1,True),(0,True)]),  # (x,y)->(1-y,1-x)
-        ],
-    )
-
-def get_peano_curve():
-    """Example of fractal curve due to G.Peano."""
-    id_map = BaseMap([(0,False),(1,False)])
-    x_map  = BaseMap([(0,True),(1,False)])  # (x,y)->(1-x,y)
-    y_map  = BaseMap([(0,False),(1,True)])  # (x,y)->(x,1-y)
-    xy_map = BaseMap([(0,True),(1,True)])   # (x,y)->(1-x,1-y)
-    return FractalCurve(
-        dim=2, div=3,
-        entrance=(0,0), exit=(1,1),
-        proto=[(0,0),(0,1),(0,2),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)],
-        base_maps=[
-            id_map, x_map, id_map,
-            y_map, xy_map, y_map,
-            id_map, x_map, id_map,
-        ],
-    )
-
-def main():
-    # run some tests
-    for curve in [get_hilbert_curve(), get_peano_curve()]:
-        curve.check()
-    print("ok!")
-
-if __name__ == "__main__":
-    main()
---
+++ test_fractal_curve.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from fractal_curve import FractalCurve, BaseMap
+
+def get_hilbert_curve():
+    """Example of fractal curve due to D.Hilbert."""
+    return FractalCurve(
+        dim=2, div=2,
+        proto=[(0,0), (0,1), (1,1), (1,0)],
+        base_maps=[
+            BaseMap([1,0],[False,False]),  # (x,y)->(y,x)
+            BaseMap.id_map(dim=2),  # (x,y)->(x,y)
+            BaseMap.id_map(dim=2),  # (x,y)->(x,y)
+            BaseMap([1,0],[True,True]),  # (x,y)->(1-y,1-x)
+        ],
+    )
+
+def get_peano_curve():
+    """Example of fractal curve due to G.Peano."""
+    id_map = BaseMap.id_map(dim=2)
+    x_map  = BaseMap([0,1],[True,False])  # (x,y)->(1-x,y)
+    y_map  = BaseMap([0,1],[False,True])  # (x,y)->(x,1-y)
+    xy_map = BaseMap([0,1],[True,True])   # (x,y)->(1-x,1-y)
+    return FractalCurve(
+        dim=2, div=3,
+        proto=[(0,0),(0,1),(0,2),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)],
+        base_maps=[
+            id_map, x_map, id_map,
+            y_map, xy_map, y_map,
+            id_map, x_map, id_map,
+        ],
+    )
+
+# разрывная кривая - начало в центре квадрата
+def get_discontinuous_curve():
+    return FractalCurve(
+        dim=2, div=3,
+        proto=[(1,1),(0,1),(0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2)],
+        base_maps=[BaseMap.id_map(dim=2)]*9,
+    )
+
+def main():
+    # run some tests
+    for curve, name in [
+        (get_hilbert_curve(), 'Hilbert'),
+        (get_peano_curve(), 'Peano'),
+        (get_discontinuous_curve(), 'discontinuous'),
+    ]:
+        print('curve:', name)
+        print('entrance:', curve.entrance)
+        print('exit:', curve.exit)
+        try:
+            curve.check()
+            print('check ok!')
+        except Exception as exc:
+            print('check failed:', exc)
+            continue
+
+        print()
+
+if __name__ == "__main__":
+    main()
