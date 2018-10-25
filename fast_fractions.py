6622678f328706d28156ca91a4bb4e9a0ebf3546
Yuri Malykhin <jura05 at >
2018-10-25 10:41:52 +0300

    fast fractions class

---
+++ fast_fractions.py
@@ -0,0 +1,32 @@
+from math import gcd
+
+class FastFraction:
+
+    def __init__(self, n, d):
+        if d < 0:
+            n = -n
+            d = -d
+        g = gcd(n, d)
+        self.n = n // g
+        self.d = d // g
+
+    def __lt__(self, other):
+        return self.n * other.d < other.n * self.d
+
+    def __le__(self, other):
+        return self.n * other.d <= other.n * self.d
+
+    def __eq__(self, other):
+        return self.n * other.d <= other.n * self.d
+
+    def __neg__(self):
+        return FastFraction(-self.n, self.d)
+
+    def __float__(self):
+        return self.n / self.d
+
+    def __str__(self):
+        return str(float(self))
+
+    def __repr__(self):
+        return 'FastFraction({}, {})'.format(self.n, self.d)
