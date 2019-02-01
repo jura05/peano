cfb8a807017f78dfc37b55f37f71bcc6d280de86
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2019-02-01 23:27:37 +0300

    Update examples.py

--- examples.py
+++ examples.py
@@ -136,3 +136,63 @@ def get_morton_curve():
         proto=[(0, 0), (1, 0), (0, 1), (1, 1)],
         base_maps=[BaseMap(dim=2)] * 4,
     )
+
+
+# Polyfractal curve (not realized)
+
+
+# Tetrafractal curve
+def get_ARW_Curve(k):
+
+    chain_code = [['i','Ij','i'],
+                  ['jiJ'],
+                  ['jiJ'],
+                  ['jiJ']],
+    bases = [['3ij0','1jI1','2Ji0','1iJ0'],
+             ['3ji0','2Ij1','1ij0','1JI0'],
+             ['0ji0','1Ji0','0jI0','1JI0'],
+             ['0ij0','2Ji0','0jI0','3Ji1']]
+    return FractalCurve(
+        proto=chain2proto(chain_code),
+        base_maps=[basis2base_map(b) for b in bases],
+    )
+
+
+# Minimal 2D monofractal curve in L_1 (9), L_2 (5), L_inf (5)
+def get_beta_Omega_Curve():
+
+    chain_code = [['jiJ'],
+                  ['jiJ']],
+    bases = [['1iJ0','1Ji0','1ji1','1IJ1'],
+             ['1iJ0','1Ji0','1ji1','0jI0']]
+    return FractalCurve(
+        proto=chain2proto(chain_code),
+        base_maps=[basis2base_map(b) for b in bases],
+    )
+
+
+# Minimal 3D bifractal curve in L_inf (9.45)
+# Is minimal L_2 (18.3)?
+def get_neptunus_curve():
+    
+    chain_code = [['kjKikJK'],
+                  ['kiKjIki']],
+    bases = [['0kji','1kji','1KiJ','1jKI','1ikj','1KJi','0kJI','1jKI'],
+             ['0jki','1jki','1iKJ','0KiJ','1JiK','1IKj','0ikj','1ijk']]
+    return FractalCurve(
+        proto=chain2proto(chain_code),
+        base_maps=[basis2base_map(b) for b in bases],
+    )
+ 
+
+# Is minimal L_1 (89.8) and L_2 (18.3)?
+def get_luna_curve():
+    
+    chain_code = [['kjKikJK'],
+                  ['kiKjIki']],
+    bases = [['1ijk','0KJi','1KiJ','1jKI','1jik','1IKj','0kJI','1kJI'],
+             ['1jik','0JKi','1iKJ','0KiJ','1KjI','1JIk','0ikj','1ikj']]
+    return FractalCurve(
+        proto=chain2proto(chain_code),
+        base_maps=[basis2base_map(b) for b in bases],
+    )
