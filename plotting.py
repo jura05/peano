e4884001d473c56df306d17593d0ab7facbbe28f
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2019-03-12 16:36:37 +0300

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,7 +1,8 @@
 from examples import *
 import matplotlib.pyplot as plt
-from numpy import linspace, array, amin
 from mpl_toolkits.mplot3d import Axes3D
+
+sub_numb = 2
 
 #curve = get_hilbert_curve()
 #curve = get_peano_curve()
@@ -9,23 +10,31 @@ from mpl_toolkits.mplot3d import Axes3D
 #curve = get_coil_curve()
 #curve = get_serpentine_curve()
 #curve = get_R_curve()
-#curve = get_haverkort_curve_1()
-curve = get_haverkort_curve_2()
+curve = get_haverkort_curve_1()
+#curve = get_haverkort_curve_2()
 #curve = get_tokarev_curve()
 #curve = get_discontinuous_curve()
 #curve = get_morton_curve()
 
-def plot_curve(subdiv_n, dim, genus, numb_sub):
+def plot_curve(subdiv_n, dim, genus, sub_numb):
     
-    subdiv_n = array(subdiv_n)
-    subdiv_n = subdiv_n/(genus**((numb_sub+1)/dim))
+    #Масштабируем координаты, делим все значения на genus**((sub_numb+1)/dim)
+    subdiv_n = [[i/(genus**((sub_numb+1)/dim)) for i in subdiv_n[j]] for j in range(len(subdiv_n))]   
+    #Находим минимум по каждой коодинате x,y,z b и т.д., 
+    #это необходимо для кривых, у которых точки входа и выхода находятся не в вершинах 
+    max_col = [min([row[i] for row in subdiv_n]) for i in range(dim)]
+    #Сдвиг всех координат на 1/(2*genus**((sub_numb+1)/(dim))) с учетом минимальных значений по координатам
+    subdiv_n = [[subdiv_n[j][i] + max_col[i] + 1/(2*genus**((sub_numb+1)/(dim))) for i in range(dim)] for j in range(len(subdiv_n))]
     
-    for k in range(dim):
-        subdiv_n[:,k] = subdiv_n[:,k] - amin(subdiv_n[:,k]) + 1/(2*genus**((numb_sub+1)/(dim)))
-
+    #Функция создания сетки для 
+    def linspace(genus,sub_numb):
+        ticks = list(range(0,int(genus**(sub_numb/2)+1)))
+        ticks = [i/genus**(sub_numb/2) for i in ticks]
+        return ticks
+    
     if dim == 2:
 
-        ticks = linspace(0,1,genus**(numb_sub/2)+1)
+        ticks = linspace(genus,sub_numb)
     
         plt.figure()
         plt.gcf().set_size_inches(9,9)
@@ -33,11 +42,13 @@ def plot_curve(subdiv_n, dim, genus, numb_sub):
         plt.yticks(ticks,[])
         plt.grid(True)
         plt.axis([0,1,0,1])
-        plt.plot(subdiv_n[:,0],subdiv_n[:,1],'k')
+        sub1 = [row[0] for row in subdiv_n]
+        sub2 = [row[1] for row in subdiv_n]
+        plt.plot(sub1,sub2,'k')
 
     elif dim == 3:
     
-        ticks = linspace(0,1,genus**(numb_sub/3)+1)
+        ticks = linspace(genus,sub_numb)
     
         fig = plt.figure(figsize = (9,9))
         ax = fig.gca(projection='3d')
@@ -48,7 +59,10 @@ def plot_curve(subdiv_n, dim, genus, numb_sub):
         ax.set_yticks(ticks)
         ax.set_zticks(ticks)
         ax.tick_params(colors = 'w')
-        ax.plot(subdiv_n[:,0],subdiv_n[:,1],subdiv_n[:,2],'k')
+        sub1 = [row[0] for row in subdiv_n]
+        sub2 = [row[1] for row in subdiv_n]
+        sub3 = [row[2] for row in subdiv_n]
+        ax.plot(sub1,sub2,sub3,'k')
 
-numb_sub = 1
-plot_curve(curve.get_subdivision(k=numb_sub).proto, curve.dim, curve.div**curve.dim, numb_sub)
+
+plot_curve(curve.get_subdivision(k=sub_numb).proto, curve.dim, curve.div**curve.dim, sub_numb)
